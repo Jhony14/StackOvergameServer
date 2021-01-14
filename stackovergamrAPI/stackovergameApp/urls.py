cb300970f4e6b7b163be317b6512aa53f176c13e
Jhonatan <jhonyart14@gmail.com>
2021-01-14 23:50:02 +0100

    Doing login

--- stackovergamrAPI/stackovergameApp/urls.py
+++ stackovergamrAPI/stackovergameApp/urls.py
@@ -23,6 +23,9 @@ urlpatterns = [
     url(r'^valoracioncomentarios/$', views.valoracioncomentariosApi),
     url(r'^valoracioncomentarios/([0-9]+)$', views.valoracioncomentariosApi),
 
-    url(r'^savefile$', views.SaveFile)
+    url(r'^savefile$', views.SaveFile),
+
+    url(r'^login/$', views.login),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
